54694d2b9b6bbe93a916fb3337818aaeb61f85d2
George <egor-kulikov@mail.ru>
2023-04-06 19:57:57 +0300

    checkpoint 05.04.2023

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "../styles/App.css";
+import Header from "./Header";
+import Main from "./Main";
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Hello!</h1>
-            </div>
-        )
+            <React.Fragment>
+                <Header/>
+                <Main/>
+            </React.Fragment>
+
+        );
     }
 }
 
---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Main.css";
+
+class Main extends React.Component {
+    render() {
+        return (
+            <main>
+                <div>
+                    <h1>Hello!</h1>
+                </div>
+            </main>
+        )
+    }
+}
+
+export default Main;
